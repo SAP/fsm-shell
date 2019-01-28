e738278c8c1dff227b37ebbbb61e41d6abad2a3c
Andrey Savka <a_sawka@yahoo.com>
2019-01-28 21:15:56 +0300

    [CPB-30089] - shell client - output build version (#2)

---
+++ scripts/version.js
@@ -0,0 +1,26 @@
+
+const gitRev = require('git-rev');
+const path = require('path');
+
+function gitRevAsync() {
+  return new Promise(gitRev.short);
+}
+
+function getVersion() {
+  return gitRevAsync().then(sha => {
+
+    const packageJson = require(path.resolve('./package.json'));
+
+    const version = packageJson.version;
+    const timestamp = Math.round(new Date().getTime()/1000).toString(10);
+
+    return `${version}-rc0-${sha}-t${timestamp}`;
+  });
+}
+
+getVersion()
+  .then(console.log)
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
