62c5b02b54f6c3d2c0972c0ec11b746dbd8fd43b
Sébastien Barbier <sebastien.barbier@sap.com>
2020-03-24 13:23:09 +0100

    Remove uuid dependancies

--- src/ShellSdk.ts
+++ src/ShellSdk.ts
@@ -1,9 +1,14 @@
 
 import { EventType, SHELL_EVENTS } from './ShellEvents';
 import { SHELL_VERSION_INFO } from './ShellVersionInfo';
-import { v4 as uuidv4 } from 'uuid';
-
 import { Debugger } from './Debugger';
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
 export class ShellSdk {
 
