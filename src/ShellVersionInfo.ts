b72a1e7f2d0d8f9a65f0fdc4f46b5834b4481023
Tom Lennart Krupp <tom.lennart.krupp@sap.com>
2020-05-12 14:40:50 +0200

    adding ts lint rules from FSM and code format

--- src/ShellVersionInfo.ts
+++ src/ShellVersionInfo.ts
@@ -2,7 +2,6 @@
 // MANUAL CHANGES TO THIS FILE WILL BE OVERWRITTEN !!!
 
 export const SHELL_VERSION_INFO = {
-VERSION : 'Will-Be-Replaced-During-Build',
-BUILD_TS: 'Will-Be-Replaced-During-Build'
+  VERSION: '1.3.0',
+  BUILD_TS: '2020-05-12T12:17:37.697Z',
 };
-
