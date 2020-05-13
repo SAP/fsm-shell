526a85c0d7f8be0640f504a49e5d4d2c9b2c54cd
Sébastien BARBIER <contact@sebastienbarbier.com>
2020-05-13 08:47:29 +0200

    Update ShellVersionInfo.ts

--- src/ShellVersionInfo.ts
+++ src/ShellVersionInfo.ts
@@ -2,6 +2,6 @@
 // MANUAL CHANGES TO THIS FILE WILL BE OVERWRITTEN !!!
 
 export const SHELL_VERSION_INFO = {
-  VERSION: '1.3.0',
-  BUILD_TS: '2020-05-12T12:17:37.697Z',
+  VERSION: 'Will-Be-Replaced-During-Build',
+  BUILD_TS: 'Will-Be-Replaced-During-Build',
 };
